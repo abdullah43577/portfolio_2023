de0f303e43e1972ba7e5a01c2d6f2231686c9e6c
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-12-11 19:18:53 +0100

    made some changes

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -414,11 +414,9 @@ export default function About() {
               <div className=" text-zinc-600 font-medium dark:text-white">Linkedin</div>
             </a>
 
-            <a href="https://www.linkedin.com/in/ayo540/" target="_blank" rel="noreferrer" className="flex items-center gap-2 px-6 py-2 border border-gray-200 rounded-md hover:bg-zinc-200 dark:bg-zinc-800 dark:border-gray-800">
+            <a href="https://www.buymeacoffee.com/ayo540" target="_blank" rel="noreferrer" className="flex items-center gap-2 px-6 py-2 border border-gray-200 rounded-md hover:bg-zinc-200 dark:bg-zinc-800 dark:border-gray-800">
               <label htmlFor="myCheckBox" className="cursor-not-allowed">
-                <svg fill="#0077b5" xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
-                  <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
-                </svg>
+                <img src="https://cdn.buymeacoffee.com/assets/logos/xmas-bmc-logo-1.5.png" alt="buy me a coffee icon" className="w-[14px]" />
               </label>
               <div className=" text-zinc-600 font-medium dark:text-white">Buy me a Coffee</div>
             </a>
