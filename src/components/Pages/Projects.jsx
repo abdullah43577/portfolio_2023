ffd28de4ceb1e57ea511e77a68054bb6c22cfb2e
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-12-11 16:57:56 +0100

    added react-spinners

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 const { VITE_API_SERVER } = import.meta.env;
 import useFetch from '../hooks/useFetch';
 import useFadeUpEffect from '../hooks/useFadeUpEffect';
+import ReactLoader from '../helper/loader';
 
 export default function Projects() {
   const [projects, setProjects] = useState([]);
@@ -30,23 +31,20 @@ export default function Projects() {
     FetchProjects();
   }, []);
 
-  const project =
-    projects.length > 0
-      ? projects.map((data, i) => {
-          return (
-            <Link key={i} to={data._id} className="border border-transparent bg-zinc-50 flex items-center gap-3 hover:border-gray-200 cursor-pointer p-4 rounded-lg">
-              <div className="bg-gray-100 p-2 rounded-md min-w-[60px] min-h-[60px] flex items-center justify-center">
-                <img src={data.logo} alt="" className="w-[40px]" />
-              </div>
+  const project = projects?.map((data, i) => {
+    return (
+      <Link key={i} to={data._id} className="border border-transparent bg-zinc-50 flex items-center gap-3 hover:border-gray-200 cursor-pointer p-4 rounded-lg">
+        <div className="bg-gray-100 p-2 rounded-md min-w-[60px] min-h-[60px] flex items-center justify-center">
+          <img src={data.logo} alt="" className="w-[40px]" />
+        </div>
 
-              <div>
-                <h4 className="font-medium mb-2">{data.title}</h4>
-                <p className="text-sm">{data.tagline}</p>
-              </div>
-            </Link>
-          );
-        })
-      : 'No Data Fetched';
+        <div>
+          <h4 className="font-medium mb-2">{data.title}</h4>
+          <p className="text-sm">{data.tagline}</p>
+        </div>
+      </Link>
+    );
+  });
 
   return (
     <section ref={element} className="fade translate-y-[100px] opacity-5">
@@ -56,7 +54,7 @@ export default function Projects() {
           Among countless projects, these are my proudest achievements — <span className="italic">a testament to my dedication and creativity.</span>
         </p>
 
-        <section className="projects">{project}</section>
+        <section className={`projects ${project.length <= 0 ? 'flex' : 'grid'} items-center justify-center`}>{project.length > 0 ? project : <ReactLoader />}</section>
       </div>
     </section>
   );
